49375329ea14ef4ae89754ec8cf0d4afb4852a5d
Mahmoud <devmahmoud.me@gmail.com>
2025-10-19 14:43:27 +0300

    feat(dashboard): add dashboard layout with dual sidebar setup and providers

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+import ActivitiesSidebar from "@/components/activities/activities-sidebar";
+import { ActivitiesSidebarProvider } from "@/components/activities/activities-sidebar-provider";
+import { AppSidebar } from "@/components/layout/app-sidebar";
+import DashboardHeader from "@/components/layout/dashboard-header";
+
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <ActivitiesSidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <div className="flex">
+            <div className="flex-1">
+              <DashboardHeader />
+              <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                {children}
+              </div>
+            </div>
+            <ActivitiesSidebar />
+          </div>
+        </SidebarInset>
+      </ActivitiesSidebarProvider>
+    </SidebarProvider>
+  );
+}
