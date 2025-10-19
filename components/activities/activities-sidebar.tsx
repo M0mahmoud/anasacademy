3bcf4593936ac03de2082142d136002171896d6e
Mahmoud <devmahmoud.me@gmail.com>
2025-10-19 14:43:16 +0300

    refactor(activities): update sidebar to use context provider with smooth animations

--- components/activities/activities-sidebar.tsx
+++ components/activities/activities-sidebar.tsx
@@ -1,19 +1,32 @@
+"use client";
+
 import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import NotificationsSection from "./notifications-section";
 import ActivitiesSection from "./activities-section";
 import ContactsSection from "./contacts-section";
+import { useActivitiesSidebar } from "./activities-sidebar-provider";
+import { cn } from "@/lib/utils";
 
 export default function ActivitiesSidebar() {
+  const { open } = useActivitiesSidebar();
+
   return (
-    <div className="w-[280px] border-l bg-white h-screen flex flex-col">
-      <ScrollArea className="flex-1 h-dvh">
-        <div className="p-6">
-          <NotificationsSection />
-          <ActivitiesSection />
-          <ContactsSection />
-        </div>
-      </ScrollArea>
+    <div
+      className={cn(
+        "bg-white h-screen flex flex-col transition-all duration-300 ease-in-out",
+        open ? "w-[280px] border-l" : "w-0 overflow-hidden"
+      )}
+    >
+      {open && (
+        <ScrollArea className="flex-1 h-dvh">
+          <div className="p-6">
+            <NotificationsSection />
+            <ActivitiesSection />
+            <ContactsSection />
+          </div>
+        </ScrollArea>
+      )}
     </div>
   );
 }
